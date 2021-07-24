c70ff53893162bbed45cf530bc8266de940303c5
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-24 12:44:38 -0700

    created roots album test for post route controller

---
+++ __tests__/rootsalbums.test.js
@@ -0,0 +1,21 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'superagent';
+import app from '../lib/app.js';
+import RootsAlbums from '../lib/models/RootsAlbums.js';
+
+describe('roots albums routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a roots album via POST', async () => {
+
+    const album = { album: 'things fall apart', recordingLabel: 'mca records', releaseYear: 1999 };
+
+    const res = await request(app).post('/api/v1/rootsalbums').send(album);
+
+    expect(res.body).toEqual(album);
+  });
+}
+);
