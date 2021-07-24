ec8a2638bd69d493283cb726a101085cb654cdd4
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-24 14:27:19 -0700

    didnt know what the problem was so deleted and recreated the test file for roots, and then ended up making corections to the model file, in the constructor, resulting in roots post controller test passing

--- __tests__/roots.test.js
+++ __tests__/roots.test.js
@@ -1,7 +1,8 @@
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
-import request from 'superagent';
+import request from 'supertest';
 import app from '../lib/app.js';
+import Roots from '../lib/models/Roots.js';
 
 
 describe('roots albums routes', () => {
@@ -10,9 +11,9 @@ describe('roots albums routes', () => {
   });
 
   it('creates a roots album via POST', async () => {
-
     const album = { album: 'things fall apart', recordingLabel: 'mca records', releaseYear: 1999 };
-    const res = await request(app).post('/api/v1/rootsalbums').send(album);
+
+    const res = await request(app).post('/api/v1/roots').send(album);
 
     expect(res.body).toEqual({
       id: '1',
@@ -21,5 +22,4 @@ describe('roots albums routes', () => {
       releaseYear: 1999
     });
   });
-}
-);
+});
