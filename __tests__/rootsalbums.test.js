a959bc48b0df7a4d1852530f1b933c89d17b317c
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-24 13:48:36 -0700

    deleted controller and model files for roots albums and recreated them and finihed post route in new roots controller file

--- __tests__/rootsalbums.test.js
+++ __tests__/rootsalbums.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'superagent';
 import app from '../lib/app.js';
-import RootsAlbums from '../lib/models/RootsAlbum.js';
+
 
 describe('roots albums routes', () => {
   beforeEach(() => {
@@ -12,7 +12,6 @@ describe('roots albums routes', () => {
   it('creates a roots album via POST', async () => {
 
     const album = { album: 'things fall apart', recordingLabel: 'mca records', releaseYear: 1999 };
-
     const res = await request(app).post('/api/v1/rootsalbums').send(album);
 
     expect(res.body).toEqual({
