85a89e974d1209f2e9a34962daf3c13610988b54
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-25 15:25:07 -0700

    created and recreated owls files and tests, etc but still receiving the same error and going to commit and move on to keep forward mobility of lab

--- __tests__/owls.test.js
+++ __tests__/owls.test.js
@@ -4,21 +4,48 @@ import request from 'superagent';
 import app from '../lib/app.js';
 import Owl from '../lib/models/Owl.js';
 
-describe('owl routes', () => {
+describe('Owls routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   it('creates an owl via POST', async () => {
     const owl = { owl: 'burrowing owl', habitat: 'grasslands', threats: 'habitat loss, pesticides' };
-        
     const res = await request(app).post('/api/v1/owls').send(owl);
 
     expect(res.body).toEqual({
+      id: '1',
+      ...owl
+    });
+
+  });
+
+  it('gets all owls via GET', async () => {
+    const owl1 = await Owl.insert({
       id: '1',
       owl: 'burrowing owl',
       habitat: 'grasslands',
       threats: 'habitat loss, pesticides'
     });
+
+    const owl2 = await Owl.insert({
+      id: '2',
+      owl: 'eastern screech-owl',
+      habitat: 'eastern forests',
+      threats: 'deforestation'
+    });
+
+    const owl3 = await Owl.insert({
+      id: '3',
+      owl: 'elf owl',
+      habitat: 'desert scrub',
+      threats: 'habitat loss'
+    });
+
+    return request(app)
+      .get('/api/v1/owls')
+      .then((res) => {
+        expect(res.body).toEqual([owl1, owl2, owl3]);
+      });
   });
 });
